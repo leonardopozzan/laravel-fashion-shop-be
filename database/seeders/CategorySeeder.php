6c13a59994a83494c0f3a7447494f6942c3a9f2f
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-01-23 12:39:10 +0100

    created category entity

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Functions\Helpers;
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //es. powder, cream, liquid...
+        $categories = config('dataseeder.categories');
+        foreach($categories as $category){
+            $new_category = new Category();
+            $new_category->name = $category;
+            $new_category->slug = Helpers::generateSlug($new_category->name);
+            $new_category->save();
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypeSeeder::class,
             BrandSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
